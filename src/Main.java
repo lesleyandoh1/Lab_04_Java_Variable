9c61313e2888b680ec93b1feceddba2f4039d956
lesleyandoh <ackahalj@mail.uc.edu>
2025-06-09 13:07:57 -0400

    Completed pt.12

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import org.w3c.dom.ls.LSOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int numberOfKids = 12;
+        System.out.println("The number of kids in your family: " + numberOfKids);
+        boolean isRaining = true;
+        System.out.println("Is it raining? " + isRaining);
+        double gasPrice = 3.89;
+        System.out.println("The price of a gallon of gas: " + gasPrice);
+        int favNum = 4;
+        System.out.println("Your favorite number: " + favNum);
+        double shoeSize = 4.5;
+        System.out.println("Your shoe size: " + shoeSize);
+        int birthMonth = 5;
+        System.out.println("Your birth month: " + birthMonth);
+        String yourName = "Lesley Andoh";
+        System.out.println("Your name is: " + yourName);
+    }
     }
